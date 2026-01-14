ba5b0ee2285b8e710c4dfaa11ce5ebeaa7fe682d
Rocmazz <noureddine.mazzene@gmail.com>
2026-01-14 18:55:10 -0500

    Question 1 Done

--- topics/task1/js/script.js
+++ topics/task1/js/script.js
@@ -21,6 +21,26 @@ function draw() {
   //Background
   background("#000000");
 
+push();
+noStroke();
+fill("#9B0C6D");
+ellipse(200, 200, 100, 100);
+pop();
+
+// Ellipse
+push();
+noStroke();
+fill("#DF63B8");
+ellipse(125, 125, 75, 75);
+pop();
+
+// Small Ellipse
+push();
+noStroke();
+fill("#720F53");
+ellipse(60, 60, 50, 50);
+pop();
+
 //   //3 Ellipses
 //   drawEllipses();
 }
