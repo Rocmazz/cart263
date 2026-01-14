a2d3f98fa5fe3a5d1c016aaaa04f37390f5ff469
Rocmazz <noureddine.mazzene@gmail.com>
2026-01-14 18:54:27 -0500

    Started task 1

---
+++ topics/task1/js/script.js
@@ -0,0 +1,49 @@
+/**
+ * Task 1
+ * Noureddine Mazzene
+ *
+ *
+ */
+
+"use strict";
+
+/**
+ * Creates the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
+}
+
+/**
+ * Sets the background, draws the landscape
+ */
+function draw() {
+  //Background
+  background("#000000");
+
+//   //3 Ellipses
+//   drawEllipses();
+}
+
+// function drawEllipses() {
+//   // Big Ellipse
+//   push();
+//   noStroke();
+//   fill("#9B0C6D");
+//   ellipse(200, 200, 100, 100);
+//   pop();
+
+//   // Ellipse
+//   push();
+//   noStroke();
+//   fill("#DF63B8");
+//   ellipse(125, 125, 75, 75);
+//   pop();
+
+//   // Small Ellipse
+//   push();
+//   noStroke();
+//   fill("#720F53");
+//   ellipse(60, 60, 50, 50);
+//   pop();
+// }
